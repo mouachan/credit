0d881f11c0bb8cdb449e539d0f2e8cb5735320e2
mouachan <mourad.ouachani@gmail.com>
2020-04-10 14:10:14 +0200

    to java 8

---
+++ notation/manifest/config.js
@@ -0,0 +1,6 @@
+"use strict";
+
+    var ENV = {
+      baseUrl: "http://dmn-svc-notation-credit.apps.ocp4.ouachani.net",
+      companiesUrl: "http://companies-svc-credit.apps.ocp4.ouachani.net"
+    }
